bf87e875318636a19c3357053310eb3c163ae08c
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2024-07-10 15:37:24 +0700

    menu -> game transition

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour{
     public void PlayGame()
     {
         // Load the game scene
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
     }
     
     public void QuitGame()
